180cd73526f26a872642670498becb0298a6b223
CaptainSquid9 <83174650+CaptainSquid9@users.noreply.github.com>
2025-02-28 22:45:17 -0600

    Update root

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./", // Force relative paths
+  base: "/", // Force relative paths
   plugins: [react()],
 });
