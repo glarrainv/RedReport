213825345a25dc4c25bec1710698ae6c8dd7804b
CaptainSquid9 <83174650+CaptainSquid9@users.noreply.github.com>
2025-02-28 23:21:33 -0600

    try

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,8 +5,8 @@ import ReactMap from "./Map.tsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <>
+  <React.StrictMode>
     <App />
     <ReactMap />
-  </>
+  </React.StrictMode>
 );
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,6 +3,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/", // Force relative paths
   plugins: [react()],
 });
