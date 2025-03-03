a09e2ec27097a731302ff2e790882b62427b6204
CaptainSquid9 <83174650+CaptainSquid9@users.noreply.github.com>
2025-03-02 22:19:03 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ admin.initializeApp({
   credential: admin.credential.cert({
     project_id: process.env.FIREBASE_PROJECT_ID,
     private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    private_key: process.env.FIREBASE_PRIVATE_KEY,
     client_email: process.env.FIREBASE_CLIENT_EMAIL,
     client_id: process.env.FIREBASE_CLIENT_ID,
     auth_uri: process.env.FIREBASE_AUTH_URI,
@@ -24,7 +24,6 @@ admin.initializeApp({
     universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
   }),
 });
-console.log(process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")); // Just for debugging (remove in production)
 const db = getFirestore();
 
 // API Route to Fetch Data
