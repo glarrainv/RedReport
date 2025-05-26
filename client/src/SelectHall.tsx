890a1e9ff471d336674a0dff766a1d8c2060640c
CaptainSquid9 <83174650+CaptainSquid9@users.noreply.github.com>
2025-05-26 22:26:30 +0800

    Update SelectHall.tsx

--- client/src/SelectHall.tsx
+++ client/src/SelectHall.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import "animateu.css";
+import "./animateu.css";
 function App() {
   const navigate = useNavigate();
   // TO DO:
